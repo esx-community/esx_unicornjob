d2f0e07fd4510027669c8666f5c92dced78da17a
LuaDeldu <luadelduwaht@outlook.fr>
2018-01-18 15:54:13 +0100

    chore(version): Increment version

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX Unicorn Job'
 
-version '1.0.6'
+version '1.1.0'
 
 client_scripts {
   '@es_extended/locale.lua',
